7784667d64a51b6ea35cb2c17469d5abeea5f748
mwcz <mwc@clayto.com>
2016-12-04 21:46:51 -0500

    fix cdn paths to lodash and three.js

--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
+import * as _ from 'lodash';
 import config from './config';
 
 export default class Game {
     constructor() {
-        console.log('start game!');
         console.log('the answer to life, the universe, and everything is: ', config.answerToLifeTheUniverseAndEverything);
     }
 }
